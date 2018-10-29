e21d594ad7773c83850d9a71139a0f55a2eddd3b
ssamuel56 <samuelkirkendoll88@gmail.com>
2018-10-29 14:41:12 -0400

    Now testing for 3 pairs of students

---
+++ random_pairing.rb
@@ -0,0 +1,14 @@
+def student_sample(students)
+  ary = []
+  pairs_to_be_paired = students.shuffle.each_slice(2)
+  pairs_to_be_paired.each do |x|
+    # p x
+    if x.length == 2
+      ary << x
+    else
+      ary[rand(ary.length)].concat(x)
+    end
+  end
+  p ary
+  return ary
+end
---
+++ random_pairing_tdd.rb
@@ -0,0 +1,24 @@
+require_relative 'random_pairing.rb'
+require 'minitest/autorun'
+
+class Test_for_random_pairing < Minitest::Test
+  def test_1for1
+    assert_equal(1,1)
+  end
+  def test_for_first_pair
+    pairs = student_sample(["Sam", "Steven"])
+    assert_equal(1, pairs.count)
+  end
+  def test_for_2_pairs
+    pairs = student_sample(["Sam", "Steven", "Tracey", "Billyjack"])
+    assert_equal(2, pairs.count)
+  end
+  def test_for_2_pairs_w_extra
+    pairs = student_sample(["Sam", "Steven", "Tracey", "Billyjack", "Garry"])
+    assert_equal(2, pairs.count)
+  end
+  def test_for_3_pairs_
+    pairs = student_sample(["Sam", "Steven", "Tracey", "Billyjack", "Garry", "Josh"])
+    assert_equal(2, pairs.count)
+  end
+end
